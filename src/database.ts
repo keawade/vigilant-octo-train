0115d902f5dea30fd68c9f722d4435b1c1fe68d3
Keith Wade <keith@keithwade.com>
2025-03-05 13:00:36 -0500

    Rename generic variable to avoid confusion with TS Record

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,17 @@
 import type { Receipt } from "./receiptSchema";
 
-export interface SimpleDatabase<Record> {
-  get: (id: string) => Promise<Record | undefined>;
-  set: (id: string, value: Record) => Promise<void>;
+export interface SimpleDatabase<T> {
+  get: (id: string) => Promise<T | undefined>;
+  set: (id: string, value: T) => Promise<void>;
 }
-export class Database<Record> implements SimpleDatabase<Record> {
-  store = new Map<string, Record>();
+export class Database<T> implements SimpleDatabase<T> {
+  store = new Map<string, T>();
 
-  async get(id: string): Promise<Record | undefined> {
+  async get(id: string): Promise<T | undefined> {
     return this.store.get(id);
   }
 
-  async set(id: string, value: Record): Promise<void> {
+  async set(id: string, value: T): Promise<void> {
     this.store.set(id, value);
   }
 }
